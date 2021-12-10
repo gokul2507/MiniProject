2e89522fb7cd40268370ba9315d04f340bd1543e
gokul2507 <srigokulrajendiran@gmail.com>
2021-12-10 15:48:33 +0530

    -

--- w1/urls.py
+++ w1/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from app import views
 from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
